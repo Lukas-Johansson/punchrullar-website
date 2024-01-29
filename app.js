2c08e038b7caff7b048af11316f29723e324717c
= <=>
2024-01-29 13:23:09 +0100

    Refactor app.js and index.html

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const mysql = require('mysql');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const pool = require('./db')
 
-const app = express();
-const port = 3001;
+const app = express()
+const port = process.env.PORT || 3001
 
-const dbConfig = {
-  host: 'jupiter.umea-ntig.se',
-  user: 'lukas',
-  password: 'potatis204',
-  database: 'punchrulle',
-  connectionLimit: 1000,
-  connectTimeout: 60 * 60 * 1000
-};
-
-const pool = mysql.createPool(dbConfig);
-
-app.use(cors());
-app.use(express.json());
+app.use(cors();
 
 app.get('/punchrullar/count', (req, res) => {
   pool.query('SELECT remaining FROM punchrullar', (error, results) => {
@@ -55,17 +44,3 @@ app.post('/punchrullar/update', (req, res) => {
             res.status(500).json({ error: 'Internal Server Error' });
           } else {
             res.json({ message: `Punchrullar count ${action === 'add' ? 'added' : 'removed'} successfully`, data: fetchResults[0] });
-          }
-        });
-      }
-    });
-  } else {
-    res.status(400).json({ error: 'Invalid request. Please provide a valid "action" parameter.' });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
-
-
